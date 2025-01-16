f9266f00828e3242e45b7e4815f5ff0ae7d0ec51
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-01-16 18:00:51 -0300

    chore(people): add sample data for development and test environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Este arquivo deve garantir a existência dos registros necessários para rodar a aplicação em todos os ambientes (produção,
+# desenvolvimento, teste). O código aqui deve ser idempotente para que possa ser executado em qualquer ponto de qualquer ambiente.
+# Os dados podem então ser carregados com o comando bin/rails db:seed (ou criados junto com o banco de dados com db:setup).
+
+if Rails.env.development? || Rails.env.test?
+    puts "Seeding data for the #{Rails.env} environment..."
+
+    Person.destroy_all
+
+    # Criando registros fictícios
+    people = [
+    { name: 'John Doe', cpf_cnpj: '12345678901', birth_date: '1990-01-15', phone: '123456789', email: 'johndoe@example.com', person_type: :physical },
+    { name: 'Acme Corp', cpf_cnpj: '98765432100123', birth_date: '2000-05-20', phone: '987654321', email: 'contact@acmecorp.com', person_type: :legal },
+    { name: 'Jane Smith', cpf_cnpj: '12345678902', birth_date: '1985-07-10', phone: '123123123', email: 'janesmith@example.com', person_type: :physical },
+    { name: 'Global Inc', cpf_cnpj: '65432109876543', birth_date: '1995-03-12', phone: '456456456', email: 'info@globalinc.com', person_type: :legal }
+    ]
+
+    # Criando os registros no banco de dados
+    people.each do |person|
+    Person.create!(person)
+    end
+
+    puts "Seeded #{Person.count} people."
+end
