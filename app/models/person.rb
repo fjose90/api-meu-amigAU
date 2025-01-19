49121913b68735ccad61ef934f10a66164d78717
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-01-19 11:02:50 -0300

    feat(person): add validations and tests for Person model
    - Add presence, uniqueness, and format validations for `name`, `cpf_cnpj`, `birth_date`, `email`, and `phone` attributes in `Person` model.
    - Add corresponding tests using `shoulda-matchers` to validate the presence, uniqueness, and format of attributes in `Person` model."

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,23 +1,34 @@
 class Person < ApplicationRecord
-    enum :person_type,  physical: 1, legal: 2
+  # Definição do enum para o tipo de pessoa
+  enum :person_type, physical: 1, legal: 2
 
+  # Validações
+  validates :name,
+            presence: true,
+            length: { minimum: 5, maximum: 100 }
 
-    validates :name,
-    presence: true,
-    length: { minimum: 5, maximum: 100 }
+  validates :cpf_cnpj,
+            presence: true,
+            uniqueness: true,
+            format: {
+              with: /\A\d{11,14}\z/,
+              message: 'must be 11 or 14 digits'
+            }
 
-    validates :cpf_cnpj, presence: true, format: {
-    with: /\A\d{11,14}\z/,
-    message: "must be 11 or 14 digits"
-  }
+  validates :birth_date,
+            presence: true
 
-    validates :birth_date,
-    presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP,
+                      message: 'must be a valid email address' }
 
+  validates :person_type,
+            presence: true
 
-    validates :email,
-     presence: true,
-     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
-
-    validates :person_type, presence: true
+  validates :phone,
+            presence: true,
+            format: { with: /\A\(\d{2}\) \d{4,5}-\d{4}\z/,
+                      message: 'deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX' }
 end
